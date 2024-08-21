c34ec080f023bbaa9975bd9474fa6b36d8f92137
mik-305 <mik-305@bk.ru>
2024-08-21 09:30:35 +0300

    Add files via upload

--- fake_math.py
+++ fake_math.py
@@ -1,9 +1,7 @@
 def divide(first, second):
     if second == 0:
-        print('Делить на 0 нельзя!')
-    else:
-        print('Результат деления:  ',first / second)
-    return
+        return 'Делить на 0 нельзя!'
+    return first / second
 
 
 
